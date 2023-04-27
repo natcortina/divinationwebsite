5c2386ad570822dd8cc90d85b39d19cc843cc000
natcortina <natcortinab@gmail.com>
2023-04-26 21:13:52 -0400

    pages 1 and 2 edit

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
 let btn = document.querySelector('button');
-
-let input; 
+let input;
 
 var choice = document.getElementById("choice");
 
 choice.addEventListener("keypress", function(event) {
-  
   if (event.key === "Enter") {
     event.preventDefault();
     btn.click();
   }
 });
 
-
 btn.addEventListener('click', ()=> {
-    let form = document.getElementById("form")
+    let form = document.getElementById("form");
+    input = form.elements[0].value;
 
-    input = form.elements[0].value
+  
+    if (isNaN(input) || input < 1 || input > 222) {
 
+        document.getElementById("result").textContent = "Invalid input. Please enter a number between 1 and 222.";
 
-    async function getAdvice(input) {
+    } else {
 
-    let api_url = "https://api.adviceslip.com/advice/search/" + input
-    
-    
-      const response = await fetch(api_url);
-      const data = await response.json();
+        async function getAdvice(input) {
 
-console.log(data)
-      if (data.message != null) {
+            let api_url = "https://api.adviceslip.com/advice/" + input;
+            const response = await fetch(api_url);
+            const data = await response.json();
 
-
-           document.getElementById("result").textContent = "Try Again!!"
-      } 
-
-      else {
- const advice = data.slips[0].advice
-        document.getElementById("result").textContent = " " + advice 
-     
-
-      }
-    }    
-    getAdvice(input); 
-})
+            if (data.message != null) {
+                document.getElementById("result").textContent = "Try Again!!";
+            } else {
+                const advice = data.slip.advice;
+                document.getElementById("result").textContent = " " + advice;
+            }
+        }
+        getAdvice(input); 
+    }
+});
 
 
 
